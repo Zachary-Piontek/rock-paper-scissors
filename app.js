a64124805537c76b1d62b715a33c0b8e0b5c5262
Zachary Piontek <fortunefavorsthebrave@protonmail.com>
2022-06-07 22:29:21 -0700

    added scoring function

--- app.js
+++ app.js
@@ -4,15 +4,52 @@ import { getRandomItem, score } from './utils.js';
 
 // state
 
+const threw = ['rock', 'paper', 'scissors'];
+let computerThrew = '';
+let userThrew = '';
+
+
+const rockButton = document.getElementById('rock-button');
+const paperButton = document.getElementById('paper-button');
+const scissorsButton = document. getElementById('scissor-button');
 // components
     // component
+function gameOutcome(){
+    computerThrew = getRandomItem(threw);
+    const result = score(userThrew, computerThrew);
+
+    if (result === -1) {
+        losses += 1;
+    }
+    else if (result === 0) {
+        draws += 1;
+    }
+    else if (result === 1) {
+        wins += 1;
+    }
+}
     // define and grab DOM elements
     // display functions
     // optional: subscribe to events
         // event handlers - what needs to happen?
         // logic and calculations
         // state update
+
+
+
         // re-display components (which ones?)
+rockButton.addEventListener('click', () => {
+    gameOutcome('rock');
+});
+paperButton.addEventListener('click', () => {
+    gameOutcome('paper');
+});
+scissorsButton.addEventListener('click', () => {
+    gameOutcome('scissors');
+});
+
+
+
     // optional: handle functions for shared event handler logic
 
 // page load actions
